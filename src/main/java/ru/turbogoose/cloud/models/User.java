b061e1e127a5e01cfb8fe525a4e5403568943410
ilakonovalov <antonpuckich@gmail.com>
2023-09-12 13:24:00 +0300

    Configure flyway migration for postgres

--- src/main/java/ru/turbogoose/cloud/models/User.java
+++ src/main/java/ru/turbogoose/cloud/models/User.java
@@ -1,7 +1,6 @@
 package ru.turbogoose.cloud.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "username", unique = true)
-    @Size(min = 2, max = 100, message = "Username length must be in between 2 and 100 symbols")
+    @Size(min = 2, max = 50, message = "Username length must be in between 2 and 50 symbols")
     private String username;
     @Column(name = "password")
-    @Min(value = 5, message = "Password must have at least 5 symbols")
+    @Size(min = 5, max = 100, message = "Password length must be in between 5 and 100 symbols")
     private String password;
 
     public User(String username, String password) {
