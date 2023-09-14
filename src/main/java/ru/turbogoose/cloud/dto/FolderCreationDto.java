ee0f143933a2ee3d5256b70243226a6ed03d0621
ilakonovalov <antonpuckich@gmail.com>
2023-09-14 18:33:41 +0300

    Fix regexps

--- src/main/java/ru/turbogoose/cloud/dto/FolderCreationDto.java
+++ src/main/java/ru/turbogoose/cloud/dto/FolderCreationDto.java
@@ -6,6 +6,6 @@ import lombok.Data;
 @Data
 public class FolderCreationDto {
     private String parentFolderPath;
-    @Pattern(regexp = "^[\\w !.*+'\\[\\]()\\-/]+$", message = "Folder name must not contain unsupported characters")
+    @Pattern(regexp = "^[\\w !.*+'\\[\\]()\\-]+$", message = "Folder name must not contain unsupported characters")
     private String newFolderName;
 }
