2c25c21b3f7bcadd929bc213d12003907eef3a8a
ilakonovalov <antonpuckich@gmail.com>
2023-09-19 15:04:47 +0300

    Refactor controller advice to handle all exceptions and log them

--- src/main/java/ru/turbogoose/cloud/controllers/advice/GlobalExceptionHandler.java
+++ src/main/java/ru/turbogoose/cloud/controllers/advice/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
-package ru.turbogoose.cloud.controllers;
+package ru.turbogoose.cloud.controllers.advice;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,16 +12,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import static ru.turbogoose.cloud.utils.PathUtils.getPathParam;
 
 @ControllerAdvice
-public class FileSizeExceptionControllerAdvice {
+public class GlobalExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @Value("${spring.servlet.multipart.max-file-size:10MB}")
     private String MAX_UPLOAD_SIZE;
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public String handleMaxUploadException(HttpServletRequest request,
                                            RedirectAttributes redirectAttributes) {
-        String path = request.getParameter("path");
+        LOGGER.warn("Max upload size exceeded: {}", MAX_UPLOAD_SIZE);
         redirectAttributes.addFlashAttribute("failureAlert",
                 "Uploaded object size must not exceed " + MAX_UPLOAD_SIZE);
+        String path = request.getParameter("path");
         return "redirect:/" + getPathParam(path);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public String logAllUnhandledExceptions(Exception exception) {
+        LOGGER.error("An error occurred:", exception);
+        return "redirect:/error";
     }
 }
